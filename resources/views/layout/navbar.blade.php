72f69f09f7de9ee663dfd50f944bdf99f5f459ef
TajulMunandar <tajulmunandar701@gmail.com>
2023-08-08 13:03:23 +0700

    login

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar p-4 ">
     <div class="container-fluid px-5">
         <a class="navbar-brand text-light fw-bold" href="/">Stasiun Meteorologi </a>
-        {{-- @if (auth()->user())
+        @if (auth()->user())
             <div class="d-flex">
                 <div class="dropdown">
-                    <button class="btn btn-login dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button class="btn btn-login dropdown-toggle text-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         {{ auth()->user()->name }}
                     </button>
                     <ul class="dropdown-menu">
@@ -15,10 +15,10 @@
                     </ul>
                   </div>
             </div>
-        @else --}}
+        @else
             <div class="d-flex">
                 <a class="btn btn-login text-light" href="/login">Login</a>
             </div>
-        {{-- @endif --}}
+        @endif
     </div>
 </nav>
